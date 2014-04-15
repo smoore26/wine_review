6abba229ad543a8967e81bdfbdfd3917deed6924
Stephanie Moore <blessedsmc2002@yahoo.com>
2014-04-15 14:33:03 -0400

    HWupdate

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,12 @@
 class Wine < ActiveRecord::Base
+VARIETALS = ['Restylane', 'Zinfindel', 'Merlot', 'Pinot Nior', ]
+scope :bargains, -> { where('price < 25.00') }
+scope :by, ->(author) { where('varietal = ?', varietal) }
+validates :name, :year, :country, :varietal, presence: true
+validates :year,
+numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+unless: "year.blank?"
+    validates :price,
+    numericality: { greater_than_or_equal_to: 0 },
+    if: "price.present?"
 end
